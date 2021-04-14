0d4ced65da17bd7013d5934ec0019a12e1a8694c
George <geo264@mail.ru>
2021-04-15 00:34:21 +0300

    file error

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync');
-const sass        = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
-const rename = require("gulp-rename");
-const htmlmin = require('gulp-htmlmin');
-const imagemin = require('gulp-imagemin');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require("gulp-rename");
+var htmlmin = require('gulp-htmlmin');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('server', function() {
     browserSync({
@@ -30,10 +30,34 @@ gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
     gulp.watch("src/*html").on("change", gulp.parallel('html'));
 }); // следит за изменениями файлов
-gulp.task('html', function() {
+gulp.task('html', function() { //если изменяется фаил html она обновляется с бомощью браузера синк и перемещается в папку dist
     return gulp.src("src/*.html")
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest("dist/"));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles')); // объедилинили первые две команды, чтобы запускать по умолчанию одновременно
+gulp.task('scripts', function() { 
+    return gulp.src("src/js**/*.js")
+        .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('fonts', function() {
+    return gulp.src("src/fonts**/*")
+        .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task('img', function() { 
+    return gulp.src("src/img**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task('mailer', function() { 
+    return gulp.src("src/mailer**/*")
+        .pipe(gulp.dest("dist/mailer"));
+});
+// загружаем все файлы в dist и ничего не сработало, точнее картинки не видны, но он картинки отдельно закинул, а иконки отдельно,
+//может в это причина??? Так как картинкам досталась свойство .pipe(imagemin()) сжимание картинки
+
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 'scripts', 'fonts', 'img', 'mailer')); // объедилинили первые две команды, чтобы запускать по умолчанию одновременно
